39cc7f68815b760dfd8848b59fc46f534988b44d
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-08-04 01:25:55 +0530

    Saving  Account Lombok Model

---
+++ src/main/java/com/swsa/model/SavingAccount.java
@@ -0,0 +1,30 @@
+package com.swsa.model;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class SavingAccount
+
+{
+    long AccountNo;
+    String AccountType;
+    String LastName;
+    String FirstName;
+    String FatherName;
+    String Gender;
+    String Address;
+    String EmailId;
+    long DOB;
+    long MobileNo;
+    int PinCode;
+    long AadhaarNo;
+
+
+
+}
+
+
+
