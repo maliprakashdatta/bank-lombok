bad8ed6936d78db53e99ec2332086a8c2341e4df
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-08-04 01:24:52 +0530

    Deposit Account Lombok Model

---
+++ src/main/java/com/swsa/model/DepositAccount.java
@@ -0,0 +1,22 @@
+package com.swsa.model;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class DepositAccount {
+
+    long AccountNo;
+    long DepositAmount;
+    int MobileNo;
+    String AccountType;
+    String LastName;
+    String FirstName;
+    String BranchName;
+    long Date;
+
+
+
+}
