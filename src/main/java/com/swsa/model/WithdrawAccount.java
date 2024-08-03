075a516025ddacc16590e8c835f1576abe95b0f4
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-08-04 01:27:08 +0530

    Withdraw Account Lombok Model

---
+++ src/main/java/com/swsa/model/WithdrawAccount.java
@@ -0,0 +1,15 @@
+package com.swsa.model;
+import lombok.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class WithdrawAccount {
+
+
+    long AccountNo;
+    double WithdrawAmount;
+
+
+}
